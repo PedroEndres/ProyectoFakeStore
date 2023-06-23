98b81be1f38f0ba69de68399c8b7f0b3b3154cf8
Pedro Endres <endrespedro@yahoo.com>
2023-06-23 16:30:53 -0300

    Cambie todo pa
    Beatiful

--- js/main.js
+++ js/main.js
@@ -1,18 +1,18 @@
 fetch("https://fakestoreapi.com/products")
-    .then(response => response.json())
-    .then(datos => {
-        let banderas = ''
-        for (let articulo of datos) {
-            banderas += `<div class="tarjeta"><img src="${articulo.image}"><br>
-                    <p>Titulo:${articulo.title}</p>
-                    <p>Categoria:${articulo.category}</p>
-                    <p>Descripcion:${articulo.description}</p>
-                    <p>Precio:${articulo.price}</p>
-                    <p>Ranking:${articulo.rating}</p>
-           
-               </div>`
-        }
-        const elemento = document.querySelector("#banderas")
-        elemento.innerHTML = banderas
-        console.log(datos)}
-    )
+  .then((response) => response.json())
+  .then((datos) => {
+    let Cartas = "";
+    for (let articulo of datos) {
+      Cartas += `<div class="tarjeta">
+                   <img src="${articulo.image}"><br>
+                   <p class="p-categoria">${articulo.category}</p>
+                   <p class="p-titulo">${articulo.title}</p>
+                   <p class="p-descripcion">${articulo.description}</p>
+                   <p class="p-precio">Price: ${articulo.price}</p>
+                   <p>${articulo.rating}</p>
+               </div>`;
+    }
+    const elemento = document.querySelector("#articulos");
+    elemento.innerHTML = Cartas;
+    console.log(datos);
+  });
